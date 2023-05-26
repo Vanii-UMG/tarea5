10df671191d2abfe2991c07f12493e5bf1a4e81c
Vanii <valcantaram@miumg.edu.gt>
2023-05-26 15:14:09 -0600

    Update: modificado el codigo imprimir

--- MatrizAdyacenciaGrafos.java
+++ MatrizAdyacenciaGrafos.java
@@ -16,11 +16,22 @@ public class MatrizAdyacenciaGrafos {
                 if (i != j) {
                     System.out.print("Peso del camino entre el vértice " + (i + 1) + " y el vértice " + (j + 1) + ": ");
                     matrizAdyacencia[i][j] = scanner.nextInt();
+                } else {
+                    matrizAdyacencia[i][j] = 0;
                 }
+
+                System.out.println("\nMatriz de adyacencia parcial:");
+                for (int k = 0; k <= i; k++) {
+                    for (int l = 0; l < numVertices; l++) {
+                        System.out.print(matrizAdyacencia[k][l] + "\t");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
             }
         }
 
-        System.out.println("\nMatriz de adyacencia generada:");
+        System.out.println("\nMatriz de adyacencia final:");
         for (int i = 0; i < numVertices; i++) {
             for (int j = 0; j < numVertices; j++) {
                 System.out.print(matrizAdyacencia[i][j] + "\t");
@@ -29,6 +40,7 @@ public class MatrizAdyacenciaGrafos {
         }
     }
 }
+
 
 
 
